8345ea781d52fbe85fe7a075fb2f1c7eb88b5513
Leonardo Abreu <abreu.leo@hotmail.com>
2018-11-18 00:29:13 -0200

    Aula: Componentes de Classe e Estado
    - Como utilizar uma classe no react-native
    - Como criar e modificar um state dentro da classe
    - DiferenÃa entre this.props e this.state
    - UtilizaÃÃo do  onPress e onLongPress

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -4,9 +4,15 @@ import {createDrawerNavigator} from 'react-navigation'
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
 import {Inverter, MegaSena} from './componentes/Multi'
+import Contador from './componentes/Contador'
 
 
 export default createDrawerNavigator({
+
+    Contador:{
+        screen: () => <Contador numeroInicial={100}/>,
+        navigationOptions: {title: 'Contador'}
+    },
     MegaSena:{
         screen: () => <MegaSena numero={6} />,
         navigationOptions: {title: 'Mega Sena'}
