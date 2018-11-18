23c2d8cdcc9e192f5ab400c469c8e1dc671a6f98
Leonardo Abreu <abreu.leo@hotmail.com>
2018-11-18 16:40:50 -0200

    Aula: Implementando Logica
    - Implementacao da logica da calculadora.

--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -35,7 +35,7 @@ export default props =>{
     if (props.triple) stylesButton.push(styles.buttonTriple)
     if (props.operation) stylesButton.push(styles.operationButton)
     return(
-        <TouchableHighlight onPress={props.onClick}>
+        <TouchableHighlight onPress={() => props.onClick(props.label)}>
             <Text style={stylesButton}>{props.label}</Text>
         </TouchableHighlight>
     )
